b45056b3f7febe0f9142286f4ff4ae6f96d21fca
AceHotel33 <lomvax@gmail.com>
2024-09-16 12:52:02 +0400

    Basic REPL setup is done

--- main.go
+++ main.go
@@ -1,9 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+var helpText string = `
+
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+
+`
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cliCommandMap = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
+func commandHelp() error {
+	fmt.Println(helpText)
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Exiting")
+	os.Exit(0)
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+
+		line := scanner.Text()
+		switch line {
+		case "help", "exit":
+			cliCommandMap[line].callback()
+		default:
+			fmt.Println("Unknown command. Type 'help' for a list of commands")
+		}
+	}
 }
